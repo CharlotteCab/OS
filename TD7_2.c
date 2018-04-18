8c72e6f54eaaa8665630c69f7f8e576a4f17a3a7
Steillia <wizleila@gmail.com>
2018-04-18 12:41:14 +0200

    18/04

--- TD7_2.c
+++ TD7_2.c
@@ -5,8 +5,26 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <time.h>
 
 int main(int argc,char** argv)
 {
-  pid_t t=malloc(argv[1],sizeof(pid_t));
+  pid_t *t=malloc(atoi(argv[1])*sizeof(pid_t));
+  int j;
+  srand(time(NULL));
+
+  for(int i=0;i<atoi(argv[1]);i++)
+  {
+    j=fork();
+    if(!j)
+    {
+      printf("hey\n");
+      sleep(rand()%10+1);
+      i=atoi(argv[1]);
+    }
+    t[i]=j;
+  }
+
+
+  return 0;
 }
